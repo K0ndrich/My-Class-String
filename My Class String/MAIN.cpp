1170cb366dd0649232787fca19fa350c78347863
K0ndrich <123kondrich@gmail.com>
2022-07-18 12:15:01 +0300

    Добавил перегрузка оператора сложения (конкатонация строк).Добавил конструктор копирования обьекта.

--- My Class String/MAIN.cpp
+++ My Class String/MAIN.cpp
@@ -22,21 +22,63 @@ public:
 
 		this->str[length] = '\0';
 	}
-	String& operator =(const char* STR)
+	//Перегрузка оператора присваивания
+	String& operator =(const String& other)
 	{
-		int length = strlen(STR);
-		delete[]str;
-		if (str != nullptr)
+		int length = strlen(other.str);
+
+		if (this->str != nullptr)
 		{
-			str = nullptr;
+			delete[]str;
 		}
-		str = new char[length + 1];
+
+		this->str = new char[length + 1];
+
 		for (int i = 0; i < length; i++)
 		{
-			this->str[i] = STR[i];
+			this->str[i] = other.str[i];
 		}
-		str[length] = '\0';
+		this->str[length] = '\0';
+
 		return *this;
+
+
+	}
+	//Коструктор копирования обьекта
+	String(const String& other)
+	{
+		int length = strlen(other.str);
+		this->str = new char[length+1];
+		for (int i = 0; i < length; i++)
+		{
+			this->str[i] = other.str[i];
+		}
+		this->str[length] = '\0';
+	}
+	//Перегрузка оператор сложения (Конкатонация строк)
+	String operator +(const String& other)
+	{
+		String Together;
+
+		int thisLength = strlen(this->str);
+		int otherLength = strlen(other.str);
+
+		Together.str = new char[thisLength + otherLength + 1];
+
+		int i = 0;
+		for (; i < thisLength; i++)
+		{
+			Together.str[i] = this->str[i];
+		}
+		for (int j = 0; j < otherLength; j++, i++)
+		{
+			Together.str[i] = other.str[j];
+		}
+
+		Together.str[thisLength + otherLength] = '\0';
+
+		return Together;
+
 	}
 	~String()
 	{
@@ -55,11 +97,13 @@ private:
 
 void main()
 {
-	String str("test");
-	String str2("second str");
+	setlocale(0, "RU");
+	String str1("1111");
+	String str2("2222");
+	String result;
+	result = str1 + str2;
 
-	str2 = str;
-	str2.Print();
+	result.Print();
 
 
 
