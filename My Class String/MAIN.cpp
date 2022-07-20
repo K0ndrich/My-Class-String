ca1cf16513892c9621761352fcf1bdd8f49d75cc
K0ndrich <123kondrich@gmail.com>
2022-07-20 12:28:02 +0300

    Добавил мето класса который подсчитывает размер строки

--- My Class String/MAIN.cpp
+++ My Class String/MAIN.cpp
@@ -2,68 +2,75 @@
 
 class String
 {
+private:
+
+	char* str;  //Указатель на строку
+	int SizeStr;  //Размер строки
+
 public:
+	//Конструтор без параметров
 	String()
 	{
 		str = nullptr;
+		SizeStr = 0;
 	}
 
-	//Обьект принимает массив char и записывает в себя
+	//Конструтор с параметрами (необходимо передать строку)
 	String(const char* STR)
 	{
-		int length = strlen(STR);
+		SizeStr = strlen(STR);
 
-		this->str = new char[length + 1];
+		this->str = new char[SizeStr + 1];
 
-		for (int i = 0; i < length; i++)
+		for (int i = 0; i < SizeStr; i++)
 		{
 			this->str[i] = STR[i];
 		}
 
-		this->str[length] = '\0';
+		this->str[SizeStr] = '\0';
 	}
-	//Перегрузка оператора присваивания
+	//Перегрузка оператора присваивания, необходим для присваивания всех значений обьекта другому)
 	String& operator =(const String& other)
 	{
-		int length = strlen(other.str);
+		SizeStr = strlen(other.str);
 
 		if (this->str != nullptr)
 		{
 			delete[]str;
 		}
 
-		this->str = new char[length + 1];
+		this->str = new char[SizeStr + 1];
 
-		for (int i = 0; i < length; i++)
+		for (int i = 0; i < SizeStr; i++)
 		{
 			this->str[i] = other.str[i];
 		}
-		this->str[length] = '\0';
+		this->str[SizeStr] = '\0';
 
 		return *this;
 
 
 	}
-	//Коструктор копирования обьекта
+	//Коструктор копирования обьекта, необходим для создания точной копии обьекта но в другой обасти памяти
 	String(const String& other)
 	{
-		int length = strlen(other.str);
-		this->str = new char[length+1];
-		for (int i = 0; i < length; i++)
+		SizeStr= strlen(other.str);
+		this->str = new char[SizeStr + 1];
+		for (int i = 0; i < SizeStr; i++)
 		{
 			this->str[i] = other.str[i];
 		}
-		this->str[length] = '\0';
+		str[SizeStr] = '\0';
 	}
-	//Перегрузка оператор сложения (Конкатонация строк)
+	//Перегрузка оператор сложения + (Конкатонация строк)
 	String operator +(const String& other)
 	{
 		String Together;
 
 		int thisLength = strlen(this->str);
 		int otherLength = strlen(other.str);
-
-		Together.str = new char[thisLength + otherLength + 1];
+		Together.SizeStr = thisLength + otherLength;
+		Together.str = new char[Together.SizeStr + 1];
 
 		int i = 0;
 		for (; i < thisLength; i++)
@@ -75,11 +82,17 @@ public:
 			Together.str[i] = other.str[j];
 		}
 
-		Together.str[thisLength + otherLength] = '\0';
+		Together.str[Together.SizeStr] = '\0';
 
 		return Together;
 
 	}
+	//Мето двозвращает длину строки (обьекта) до теминирующего \0
+	int GetSizeStr()
+	{
+		return SizeStr;
+	}
+	//Деструктор,который уничтожает обькт при выхода его из обасти видимости
 	~String()
 	{
 		delete[]str;
@@ -91,19 +104,22 @@ public:
 	}
 
 
-private:
-	char* str;
 };
 
 void main()
 {
 	setlocale(0, "RU");
-	String str1("1111");
+	String str1("111");
 	String str2("2222");
+
 	String result;
 	result = str1 + str2;
 
 	result.Print();
+	std::cout << std::endl;
+	std::cout << str1.GetSizeStr() << std::endl;
+	std::cout << str2.GetSizeStr() << std::endl;
+	std::cout << result.GetSizeStr() << std::endl;
 
 
 
